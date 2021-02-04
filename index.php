690563309433e52740024312969e5f195cb60656
luan777bro <luan.macedo@escolar.ifrn.edu.br>
2021-02-04 11:58:12 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PSI</title>
+</head>
+<body>
+    <h1>Luan Macedo</h1>
+    <p>Atividade de PSI</p>
+</body>
+</html>
